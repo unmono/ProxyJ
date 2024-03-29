87b864e674ce8f910e5487217b39a92efa7bd910
unmono <unmono.dev@gmail.com>
2024-03-29 17:55:07 +0100

    Fill everything with coments.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,15 @@ from states import ProxyState
 
 
 class ProxyStateDescriptor:
+    """
+    Makes possible to run some additional logic while getting and setting states.
+    The goal is to make possible to implement some transitional states that manage themselves while
+    their owners are, for example, being sorted.
+    Example: program can iterate over some instances until it hits one with an appropriate state. At the same
+    time there is an instance with some transitional state that supposed to be changed after timeout.
+    At the moment its state is checked, its method checks if the timeout is over and changes itself to
+    another one, so it can be picked.
+    """
 
     def __get__(self, instance, owner):
         try:
@@ -15,6 +24,9 @@ class ProxyStateDescriptor:
                                  "a '_state' attribute initialized before checking. "
                                  "It can be some default state or dynamic logic.")
         try:
+            # .self_check() is a method that supposed to run any additional logic if needed
+            # and return desired state based on it. This new state becomes an actual state
+            # of instance. In a common case it just returns self and nothing changes.
             instance._state = current_state.self_check()
         except AttributeError:
             raise AttributeError("Implementation of proxy state class has to have self_check() method.")
@@ -22,8 +34,15 @@ class ProxyStateDescriptor:
 
     def __set__(self, instance, value: ProxyState):
         try:
+            # .change_to() is a method that supposed to alter the setting of states if needed.
+            # Let's say that in a common case we want to assign some "Failed" state to instances
+            # that don't work in some way. But also we want to have an instance with some exceptional
+            # state that goes, say, "Quarantined" when we assign "Failed" to it.
+            # This way the logic of user program will be decoupled from any exceptional cases, and they
+            # will be managed by states itself.
             instance._state = instance._state.change_to(value)
         except AttributeError:
+            # If the state is not set yet, set it.
             instance._state = value
 
 
@@ -32,104 +51,123 @@ class Proxy(ABC):
 
     @property
     @abstractmethod
-    def ip(self) -> str: ...
+    def ip(self) -> str:
+        """
+        :return: IP address of a proxy
+        """
+        ...
 
 
-class ProxyServiceProtocol(Protocol):
+class ProxyProviderProtocol(Protocol):
     name: str
 
     def get_list(self) -> [Proxy]: ...
 
 
-service_protocol_error = AttributeError("Service must follow defined protocol")
+provider_protocol_error = AttributeError("Provider must follow defined protocol")
 
 
 class NoProxyLeftException(Exception):
     pass
 
 
-class NoSuchServiceWarning(UserWarning):
+class NoSuchProviderWarning(UserWarning):
     pass
 
 
 class ProxyJ:
+    """
+    Issues proxy instances based on their state.
+    Support multiple proxy providers.
+    """
 
     def __init__(
             self,
-            services: ProxyServiceProtocol | Iterable[ProxyServiceProtocol],
+            providers: ProxyProviderProtocol | Iterable[ProxyProviderProtocol],
             state_value_threshold: int
     ) -> None:
         self._pool: [Proxy] = []
-        self._switched_off: dict[str, ProxyServiceProtocol] = {}
+        self._switched_off: dict[str, ProxyProviderProtocol] = {}
         try:
-            self._services: dict[str, ProxyServiceProtocol] = {s.name: s for s in services}
+            self._providers: dict[str, ProxyProviderProtocol] = {s.name: s for s in providers}
         except AttributeError:
-            raise service_protocol_error
+            raise provider_protocol_error
         except TypeError:
             try:
-                self._services[services.name] = services
+                self._providers[providers.name] = providers
             except AttributeError:
-                raise service_protocol_error
+                raise provider_protocol_error
         self._state_threshold: int = state_value_threshold
 
     def _fill_pool(self) -> None:
         self._pool.clear()
-        for s in self._services.values():
+        for s in self._providers.values():
             self._pool.extend(s.get_list())
 
     def get_one(self) -> Proxy:
+        """
+        Issue a single proxy instance.
+        :return: ready to use proxy instance
+        """
         pool_length = len(self._pool)
         for i, p in enumerate(self._pool):
             if p.state.value < self._state_threshold:
+                # Prospective mechanism to refine pool order.
+                # It's just how it could look like, but actual implementation has to be
+                # well thought out and tested
                 if i > pool_length // 3:
                     self._mix_pool()
                 return p
         else:
             if pool_length < 1:
-                raise NoProxyLeftException("The pool is empty. Check services")
+                raise NoProxyLeftException("The pool is empty. Check providers")
             raise NoProxyLeftException("No proxy available at that moment")
 
-    def add_service(self, service: ProxyServiceProtocol) -> None:
-        self._services[service.name] = service
+    def add_provider(self, provider: ProxyProviderProtocol) -> None:
+        self._providers[provider.name] = provider
         self._fill_pool()
 
-    def del_service(self, name: str) -> None:
-        self._services.pop(name, None)
+    def del_provider(self, name: str) -> None:
+        self._providers.pop(name, None)
         self._fill_pool()
 
-    def switch_service_off(self, name: str) -> None:
+    def switch_provider_off(self, name: str) -> None:
         try:
-            self._switched_off[name] = self._services.pop(name)
+            self._switched_off[name] = self._providers.pop(name)
         except KeyError:
             if name in self._switched_off:
                 warnings.warn(
-                    message=f'The service {name} is already switched off',
-                    category=NoSuchServiceWarning,
+                    message=f'The provider {name} is already switched off',
+                    category=NoSuchProviderWarning,
                 )
             else:
                 warnings.warn(
-                    message=f'No such service {name} to switch off. At least at this given point of time.',
-                    category=NoSuchServiceWarning,
+                    message=f'No such provider {name} to switch off. At least at this given point of time.',
+                    category=NoSuchProviderWarning,
                 )
         else:
             self._fill_pool()
 
-    def switch_service_on(self, name: str) -> None:
+    def switch_provider_on(self, name: str) -> None:
         try:
-            self._services[name] = self._switched_off.pop(name)
+            self._providers[name] = self._switched_off.pop(name)
         except KeyError:
-            if name in self._services:
+            if name in self._providers:
                 warnings.warn(
-                    message=f'The service {name} is already switched on',
-                    category=NoSuchServiceWarning,
+                    message=f'The provider {name} is already switched on',
+                    category=NoSuchProviderWarning,
                 )
             else:
                 warnings.warn(
-                    message=f'No such service {name} to switch on. At least at this given point of time.',
-                    category=NoSuchServiceWarning,
+                    message=f'No such provider {name} to switch on. At least at this given point of time.',
+                    category=NoSuchProviderWarning,
                 )
         else:
             self._fill_pool()
 
     def _mix_pool(self) -> None:
+        """
+        Prospective method.
+        Reorder instances to be more efficient and prevent usage only of the first part of pool.
+        """
         pass
--- src/states.py
+++ src/states.py
@@ -4,19 +4,45 @@ from typing import Self
 
 
 class ProxyState(ABC):
+    """
+    Base class for states to use.
+    """
 
     @property
     @abstractmethod
-    def value(self) -> int: ...
+    def value(self) -> int:
+        """
+        Change to __ method?
+        :return: integer value based on which bearer of this state can be evaluated.
+        """
+        ...
 
     def self_check(self) -> Self:
+        """
+        Method which is run every time the state is being checked.
+        In a common case just returns itself. But can be used to implement
+        dynamic transitions to other states or run whatever needed logic.
+        :return: A state instance which will be assigned to the bearer of this state
+        """
         return self
 
     def change_to(self, new_state: Self) -> Self:
+        """
+        Method which is run every time when the new state is being assigned
+        to the bearer of this state. In a common case just returns the new state.
+        But can be used to implement dynamic transitions ot other states or run whatever
+        needed logic.
+        :param new_state: A state instance supposed to be assigned to the bearer of this state
+        :return: An actual state that will be assigned to the bearer of this state.
+        """
         return new_state
 
 
+# Bellow is default set of states to use:
 class Pristine(ProxyState):
+    """
+    Default state of brand-new instance ready to use
+    """
 
     @property
     def value(self) -> int:
@@ -24,6 +50,9 @@ class Pristine(ProxyState):
 
 
 class Ready(ProxyState):
+    """
+    Assigned state of instance ready to use
+    """
 
     @property
     def value(self) -> int:
@@ -31,6 +60,9 @@ class Ready(ProxyState):
 
 
 class Issued(ProxyState):
+    """
+    State of instance that is being used and not available at this moment.
+    """
 
     @property
     def value(self) -> int:
@@ -38,6 +70,10 @@ class Issued(ProxyState):
 
 
 class Exhausted(ProxyState):
+    """
+    State of instance that is not available at this moment. It was
+    used and needs time to recover.
+    """
 
     def __init__(self, timeout: int) -> None:
         self.recovery_time = time.time() + timeout
@@ -51,6 +87,10 @@ class Exhausted(ProxyState):
 
 
 class Quarantined(ProxyState):
+    """
+    State of an instance that is suspect in some failing behavior
+    and needs to be put aside for some time
+    """
 
     def __init__(self, timeout: int) -> None:
         self.recovery_time = time.time() + timeout
